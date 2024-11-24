ca60fb374da1f9d2dd7e4507085e6b0a14239a48
warren-west <teacher.warren.english@gmail.com>
2024-11-24 14:09:53 +0100

    CHORE: Populated seed data for workouts and mobility

---
+++ src/db.js
@@ -0,0 +1,434 @@
+export const workouts = [
+    PUSH = {
+        id: 1,
+        title: "Push",
+        picture: "https://images.pexels.com/photos/2486792/pexels-photo-2486792.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                workoutTitle: "Elevated side-to-side pike push ups",
+                picture: "./assets/images/exercises/pike-pushups.webp",
+                repRangeLo: 3,
+                repRangeHi: 5,
+                setRangeLo: 3,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 240,
+            },
+            {
+                workoutTitle: "Dips",
+                picture: "./assets/images/exercises/dips.webp",
+                repRangeLo: 5,
+                repRangeHi: 8,
+                setRangeLo: 4,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 180,
+            },
+            {
+                workoutTitle: "Push ups",
+                picture: "./assets/images/exercises/pushups.webp",
+                repRangeLo: 8,
+                repRangeHi: 15,
+                setRangeLo: 4,
+                setRangeHi: 6,
+                restRangeLo: 60,
+                restRangeHi: 120,
+            },
+        ]
+    },
+    PULL = {
+        id: 2,
+        title: "Pull",
+        picture: "https://images.pexels.com/photos/2268516/pexels-photo-2268516.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                workoutTitle: "10kg pull ups",
+                picture: "./assets/images/exercises/pullups.webp",
+                repRangeLo: 3,
+                repRangeHi: 5,
+                setRangeLo: 3,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 240,
+            },
+            {
+                workoutTitle: "Band chin ups",
+                picture: "./assets/images/exercises/band-pullups.webp",
+                repRangeLo: 5,
+                repRangeHi: 8,
+                setRangeLo: 4,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 180,
+            },
+            {
+                workoutTitle: "10kg Australian pull ups",
+                picture: "./assets/images/exercises/australian-pullups.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 4,
+                setRangeHi: 6,
+                restRangeLo: 60,
+                restRangeHi: 120,
+            },
+        ]
+    },
+    LEGS = {
+        id: 3,
+        title: "Legs",
+        picture: "https://images.pexels.com/photos/2158550/pexels-photo-2158550.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                workoutTitle: "10kg pistols to bench",
+                picture: "./assets/images/exercises/pistol-squats.webp",
+                repRangeLo: 4,
+                repRangeHi: 8,
+                setRangeLo: 3,
+                setRangeHi: 5,
+                restRangeLo: 60,
+                restRangeHi: 60,
+            },
+            {
+                workoutTitle: "10kg side lunges",
+                picture: "./assets/images/exercises/side-squats.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 3,
+                setRangeHi: 4,
+                restRangeLo: 60,
+                restRangeHi: 60,
+            },
+            {
+                workoutTitle: "Plyo step ups",
+                picture: "./assets/images/exercises/plyo-stepups.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 3,
+                setRangeHi: 4,
+                restRangeLo: 60,
+                restRangeHi: 60,
+            },
+            {
+                workoutTitle: "Single leg Romanian deadlifts",
+                picture: "./assets/images/exercises/single-leg-romanian-deadlift.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 3,
+                setRangeHi: 4,
+                restRangeLo: 30,
+                restRangeHi: 30,
+            }
+        ]
+    },
+    UPPER_BODY = {
+        id: 4,
+        title: "Upper body",
+        picture: "https://images.pexels.com/photos/2158402/pexels-photo-2158402.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                workoutTitle: "10kg pull ups",
+                picture: "./assets/images/exercises/pullups.webp",
+                repRangeLo: 3,
+                repRangeHi: 5,
+                setRangeLo: 3,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 240,
+            },
+            {
+                workoutTitle: "Elevated side-to-side pike push ups",
+                picture: "./assets/images/exercises/pike-pushups.webp",
+                repRangeLo: 3,
+                repRangeHi: 5,
+                setRangeLo: 3,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 240,
+            },
+            {
+                workoutTitle: "Band chin ups",
+                picture: "./assets/images/exercises/band-pullups.webp",
+                repRangeLo: 8,
+                repRangeHi: 10,
+                setRangeLo: 4,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 180,
+            },
+            {
+                workoutTitle: "Clap push ups",
+                picture: "./assets/images/exercises/clap-pushups.webp",
+                repRangeLo: 8,
+                repRangeHi: 10,
+                setRangeLo: 4,
+                setRangeHi: 5,
+                restRangeLo: 120,
+                restRangeHi: 180,
+            },
+            {
+                workoutTitle: "Australian Pull Ups",
+                picture: "./assets/images/exercises/australian-pullups.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 5,
+                setRangeHi: 8,
+                restRangeLo: 60,
+                restRangeHi: 120,
+            },
+            {
+                workoutTitle: "Band dips",
+                picture: "./assets/images/exercises/dips.webp",
+                repRangeLo: 8,
+                repRangeHi: 12,
+                setRangeLo: 5,
+                setRangeHi: 8,
+                restRangeLo: 60,
+                restRangeHi: 120,
+            }
+        ]
+    },
+]
+
+export const mobility = [
+    PULL_MOB = {
+        id: 5,
+        title: "Pull mobility",
+        picture: "https://images.pexels.com/photos/2157885/pexels-photo-2157885.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                mobTitle: "Child pose",
+                picture: "./assets/images/exercises/child.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Deep squat",
+                picture: "./assets/images/exercises/deep-squat.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Shoulder flexion w/ stick",
+                picture: "./assets/images/exercises/.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Shoulder stand",
+                picture: "./assets/images/exercises/shoulder-stand.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Lat stretch",
+                picture: [
+                    "./assets/images/exercises/lat-stretch.webp",
+                    "./assets/images/exercises/lat-stretch2.webp",
+                ],
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Pigeon pose",
+                picture: "./assets/images/exercises/pigeon.webp",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Pistol stretch",
+                picture: "./assets/images/exercises/.webp",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Frog pose",
+                picture: "./assets/images/exercises/frog.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Forward roll",
+                picture: "./assets/images/exercises/forward-roll.webp",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Wrist extension & flexion",
+                picture: "./assets/images/exercises/wrist-extension.webp",
+                duration: 60,
+                isDouble: true
+            }
+        ]
+    },
+    PUSH_MOB = {
+        id: 6,
+        title: "Push mobility",
+        picture: "https://images.pexels.com/photos/5374862/pexels-photo-5374862.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                mobTitle: "Deep squat",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Child pose",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Supinated hang",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Table top",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Triceps stretch",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Hip flexion",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Supported side split",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Forward roll",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Wrist extension & flexion",
+                duration: 60,
+                isDouble: true
+            }
+        ]
+    },
+    LEGS_MOB = {
+        id: 7,
+        title: "Legs mobility",
+        picture: "https://images.pexels.com/photos/2158400/pexels-photo-2158400.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                mobTitle: "Deep squat",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Child pose",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Laying half spinal twist",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Chest opener",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Shoulder flexion",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Shoulder extension on floor",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Upper back stretch",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Couch stretch",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Forward roll",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Calf stretch",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Wrist extension & flexion",
+                duration: 60,
+                isDouble: true
+            }
+        ]
+    },
+    UPPER_BODY_MOB = {
+        id: 8,
+        title: "Upper body mobility",
+        picture: "https://images.pexels.com/photos/25626587/pexels-photo-25626587/free-photo-of-ai-generated-shapes.jpeg?auto=compress&cs=tinysrgb&w=150",
+        exerciseList: [
+            {
+                mobTitle: "Deep squat",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Child pose",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Supinated hang",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Chest opener",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Table top",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Triceps stretch",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Hip flexion",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Supported side split",
+                duration: 60,
+                isDouble: true
+            },
+            {
+                mobTitle: "Forward roll",
+                duration: 60,
+                isDouble: false
+            },
+            {
+                mobTitle: "Wrist extension & flexion",
+                duration: 60,
+                isDouble: true
+            }
+        ]
+    },
+]
